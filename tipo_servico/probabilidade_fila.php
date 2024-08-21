b917b8c8d1f6fb5a1245d027f2a6009072dd397f
Felipe Serrao <felipe.serrao@gmail.com>
2024-08-21 18:28:36 -0300

    css

--- tipo_servico/probabilidade_fila.php
+++ tipo_servico/probabilidade_fila.php
@@ -1,25 +1,87 @@
+<style>
+    body{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            align-items: center;
+
+            margin-top: 20px;
+
+            background-color: #f5f5f5;
+        }
+        .form-area{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            align-items: center;
+
+            margin-bottom: 10px;
+
+            background-color: white;
+            padding: 15px;
+            border: 2px solid black;
+            border-radius: 5px;
+        }
+        .input{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-content: center;
+            align-items: center;
+            justify-content: flex-start;
+        }
+        .bt-final{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+        a{
+            text-decoration: none; 
+            color: black
+        }
+        .resultado{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            align-content: center;
+            align-items: center;
+
+            text-align: center;
+        }
+</style>
+
 <h1>Probabilidade de haver n clientes no sistema (Pn)</h1>
 
 <div class="form-area">
     <form action="" method="post">
-        <p>λ(taxa média de chegada de clientes): </p><input type="text" name="lambda" id="lambda">
-        <p>Por:</p>
-        <select name="unidade_lambda" id="">
-            <option value="hora">Hora</option>
-            <option value="minuto">Minuto</option>
-        </select>
-        <p>μ(taxa de atendimento):</p><input type="text" name="mi" id="mi">
-        <p>Por:</p>
-        <select name="unidade_mi" id="">
-            <option value="hora">Hora</option>
-            <option value="minuto">Minuto</option>
-        </select>
-        <p>n(número de cliente):</p><input type="text" name="valor_n" id="mi">
-        <button type="submit" name="calcular">Calcular</button>
-        <button><a href="index.php">Voltar</a></button>
+        <div class="input">
+            <p>λ(taxa média de chegada de clientes): </p><input type="text" name="lambda" id="lambda">
+            <p>Por:</p>
+            <select name="unidade_lambda" id="">
+                <option value="hora">Hora</option>
+                <option value="minuto">Minuto</option>
+            </select>
+        </div>
+        <div class="input">
+            <p>μ(taxa de atendimento):</p><input type="text" name="mi" id="mi">
+            <p>Por:</p>
+            <select name="unidade_mi" id="">
+                <option value="hora">Hora</option>
+                <option value="minuto">Minuto</option>
+            </select>
+        </div>
+        <div class="input">
+            <p>N(número de cliente):</p><input type="text" name="valor_n" id="mi">
+        </div>
+        <div class="bt-final">
+            <button type="submit" name="calcular">Calcular</button>
+            <button><a href="index.php">Voltar</a></button>
+        </div>
+        
     </form>
 </div>
-
 
 <?php
     if(isset($_POST['calcular'])){
--- tipo_servico/tempo_medio_fila.php
+++ tipo_servico/tempo_medio_fila.php
@@ -1,21 +1,82 @@
+<style>
+    body{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            align-items: center;
+
+            margin-top: 20px;
+
+            background-color: #f5f5f5;
+        }
+        .form-area{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
+            align-items: center;
+
+            margin-bottom: 10px;
+
+            background-color: white;
+            padding: 15px;
+            border: 2px solid black;
+            border-radius: 5px;
+        }
+        .input{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-content: center;
+            align-items: center;
+            justify-content: flex-start;
+        }
+        .bt-final{
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: space-evenly;
+            align-items: center;
+        }
+        a{
+            text-decoration: none; 
+            color: black
+        }
+        .resultado{
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            align-content: center;
+            align-items: center;
+
+            text-align: center;
+        }
+</style>
+
 <h1>Tempo médio no Sistema(Wq)</h1>
 
 <div class="form-area">
     <form action="" method="post">
-        <p>λ(taxa média de chegada de clientes): </p><input type="text" name="lambda" id="lambda">
-        <p>Por:</p>
-        <select name="unidade_lambda" id="">
-            <option value="hora">Hora</option>
-            <option value="minuto">Minuto</option>
-        </select>
-        <p>μ(taxa de atendimento):</p><input type="text" name="mi" id="mi">
-        <p>Por:</p>
-        <select name="unidade_mi" id="">
-            <option value="hora">Hora</option>
-            <option value="minuto">Minuto</option>
-        </select>
-        <button type="submit" name="calcular">Calcular</button>
-        <button><a href="index.php">Voltar</a></button>
+        <div class="input">
+            <p>λ(taxa média de chegada de clientes): </p><input type="text" name="lambda" id="lambda">
+            <p>Por:</p>
+            <select name="unidade_lambda" id="">
+                <option value="hora">Hora</option>
+                <option value="minuto">Minuto</option>
+            </select>
+        </div>
+        <div class="input">
+            <p>μ(taxa de atendimento):</p><input type="text" name="mi" id="mi">
+            <p>Por:</p>
+            <select name="unidade_mi" id="">
+                <option value="hora">Hora</option>
+                <option value="minuto">Minuto</option>
+            </select>
+        </div>
+        <div class="bt-final">
+            <button type="submit" name="calcular">Calcular</button>
+            <button><a href="index.php">Voltar</a></button>
+        </div>
+        
     </form>
 </div>
 
